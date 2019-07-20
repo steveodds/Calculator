1f44a72908a9740ec8e1367060df54f7a2a807c7
Stephen Odundo <steveremdi@gmail.com>
2019-07-20 19:11:19 +0300

    Basic math logic

---
+++ Math.cs
@@ -0,0 +1,33 @@
+﻿class Math
+{
+    public Math()
+    {
+
+    }
+
+    public float AddNum(float firstNumber, float secondNumber)
+    {
+        float sum = 0;
+        sum += firstNumber;
+        sum += secondNumber;
+
+        return sum;
+    }
+
+    public float SubtractNum(float firstNumber, float secondNumber)
+    {
+        float result = 0;
+        result += firstNumber;
+        result -= secondNumber;
+
+        return result;
+    }
+
+    public float MultiplyNum(float firstNumber, float secondNumber)
+    {
+        float result = 0;
+        result = firstNumber * secondNumber;
+
+        return result;
+    }
+}
