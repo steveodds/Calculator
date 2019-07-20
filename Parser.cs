f9fd245b1d33d689dc1c4592f926b56f42c428df
Stephen Odundo <steveremdi@gmail.com>
2019-07-21 00:43:44 +0300

    Added parser class to handle the calculator input and decide what gets calculated first

---
+++ Parser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Parser
+{
+    private string calculatorInput;
+    private string[] calculatorOperators;
+    private string[] splitCalculatorInput;
+    public Parser(string calculatorInput)
+    {
+        this.calculatorInput = calculatorInput;
+    }
+
+    private void OperatorScouter(string input)
+    {
+        int numberOfOperators = input.Length - input.Count(Char.IsDigit);
+        calculatorOperators = new string[numberOfOperators];
+        int arrayLocation = 0;
+        foreach (Char c in input)
+        {
+            if (!Char.IsDigit(c))
+            {
+                calculatorOperators[arrayLocation] = c.ToString();
+                arrayLocation++;
+            }
+        }
+    }
+
+    private void InputSplitter(string input)
+    {
+        splitCalculatorInput = new string[calculatorOperators.Length];
+        int nextInputIndex = 0;
+        for (int i = 0; i < calculatorOperators.Length; i++)
+        {
+            splitCalculatorInput[i] = input.Substring(nextInputIndex, input.IndexOf(calculatorOperators[i]));
+            nextInputIndex = input.IndexOf(calculatorOperators[i]) + 1;
+        }
+    }
+
+    private void OperatorPrecedence()
+    {
+
+    }
+}
